a9c6fc8b2314e0a120a3ca5f7adeb79193858ab2
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-18 11:17:23 +0530

    Startup File

---
+++ Student Attendence Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Student_Attendence_Management.Startup))]
+namespace Student_Attendence_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
