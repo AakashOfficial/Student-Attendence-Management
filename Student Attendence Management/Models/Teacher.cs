956db4989c6a1b00b42b3a3ee3de15082ecdf59d
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-18 11:28:03 +0530

    Teacher Model

---
+++ Student Attendence Management/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_Attendence_Management.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+
+        public string TeacherName { get; set; }
+
+        public string TeacherAddress { get; set; }
+
+        public int TeacherExperience { get; set; }
+
+        public int TeacherActive { get; set; }
+    }
+}
