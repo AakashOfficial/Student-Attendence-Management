6cd9e5f043f0f1a222f1bec24d72ee94e0356645
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-18 11:34:07 +0530

    Student Model

--- Student Attendence Management/Models/Student.cs
+++ Student Attendence Management/Models/Student.cs
@@ -7,5 +7,18 @@ namespace Student_Attendence_Management.Models
 {
     public class Student
     {
+        public int StudentId { get; set; }
+        
+        public string StudentName { get; set; }
+
+        public string StudentClass { get; set; }
+
+        public string StudentSection { get; set; }
+
+        public int StudentMonitor { get; set; }
+
+        public string StudentAddress { get; set; }
+
+        public string StudentActive { get; set; }
     }
 }
