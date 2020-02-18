6ee00d1a40db0eaca3d5ffb0969900f7a2764e14
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-18 11:28:33 +0530

    Student Model

---
+++ Student Attendence Management/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_Attendence_Management.Models
+{
+    public class Student
+    {
+    }
+}
