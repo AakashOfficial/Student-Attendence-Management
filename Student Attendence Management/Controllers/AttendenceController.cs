42d14cc30b5f06697ac54b56fa6fe8b4f01d284c
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-18 11:36:17 +0530

    Attendence Controller

---
+++ Student Attendence Management/Controllers/AttendenceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Attendence_Management.Controllers
+{
+    public class AttendenceController : Controller
+    {
+        // GET: Attendence
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
